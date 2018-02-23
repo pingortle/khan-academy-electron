edd0414a9e24ef85290f3e680cebad87f00c4843
Kaleb Lape <kaleb.lape@metova.com>
2018-02-23 06:08:42 -0600

    Advance through sketches with button.

--- renderer.js
+++ renderer.js
@@ -7,6 +7,8 @@ const vm = require('vm')
 const p5 = require('p5')
 new p5() // force p5 to load globally
 
+const canvas = createCanvas(0, 0)
+
 require('./lib/shims/array/rotate')
 require('./lib/shims/khan-academy')
 
@@ -19,14 +21,18 @@ const scripts = codes.map((code) => new vm.Script(code))
 const scriptOptions = { filename: 'sketch', displayErrors: true }
 
 const init = function () {
+  canvas.parent('container')
   resizeCanvas(400, 400)
 }
 setup = init
 
-setInterval(() => {
+function next() {
   setup = init
   scripts.rotate().runInThisContext(scriptOptions)
   setup()
-}, 3000)
+}
 
-scripts.rotate().runInThisContext(scriptOptions)
+const nextControl = document.getElementById('next')
+nextControl.onclick = next
+
+next()
