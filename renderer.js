8e0b69c210f3b49e007f6628fa72c264c37d0fbd
Kaleb Lape <kaleb.lape@metova.com>
2018-02-23 16:45:00 -0600

    Add packager.

--- renderer.js
+++ renderer.js
@@ -3,6 +3,7 @@
 // All of the Node.js APIs are available in this process.
 
 const fs = require('fs')
+const path = require('path')
 const vm = require('vm')
 const p5 = require('p5')
 new p5() // force p5 to load globally
@@ -12,9 +13,9 @@ const canvas = createCanvas(0, 0)
 require('./lib/shims/array/rotate')
 require('./lib/shims/khan-academy')
 
-const sketches = fs.readdirSync('./sketches')
+const sketches = fs.readdirSync(path.join(__dirname, './sketches'))
 const codes = sketches.map(
-  (filename) => fs.readFileSync(`./sketches/${filename}`).toString('utf8')
+  (filename) => fs.readFileSync(path.join(__dirname, `./sketches/${filename}`)).toString('utf8')
 )
 const scripts = codes.map((code) => new vm.Script(code))
 
